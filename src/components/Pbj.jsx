d9a3b1691af92411850f58c2a2e4b761db1f75a5
Calla Rudolph and Max Scher <calla@>
2017-09-13 09:08:58 -0700

    display 2nd step in componentWillMount method to console

--- src/components/Pbj.jsx
+++ src/components/Pbj.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 class Pbj extends React.Component {
+
   constructor(props) {
     super(props);
     console.log("1. Collect your ingredients. (2 slices bread, 1-2 butter knives, 1 jar peanut butter, 1 jar jelly or jam)");
+  }
+
+  componentWillMount() {
+    console.log("2. Open jar of peanut butter by twisting the lid counter-clockwise.");
   }
 
   render() {

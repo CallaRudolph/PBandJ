c2e7fa86f19682afb42a5cb057b669d835192d08
Calla Rudolph and Max Scher <calla@>
2017-09-13 09:05:08 -0700

    display 1st step in constructor to console

--- src/components/Pbj.jsx
+++ src/components/Pbj.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 class Pbj extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("1. Collect your ingredients. (2 slices bread, 1-2 butter knives, 1 jar peanut butter, 1 jar jelly or jam)");
+  }
+
   render() {
     return (
       <div>

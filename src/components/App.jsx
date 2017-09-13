90a3c7681c9ab381d9fab1a85eaf8d57726acd30
Calla Rudolph and Max Scher <calla@>
2017-09-13 08:59:50 -0700

    create Pbj component to render recipe steps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Pbj from "./Pbj";
 
 function App(props){
   return (
     <div>
       <h1>WHAT UP</h1>
+      <Pbj/>
     </div>
   );
 }
---
+++ src/components/Pbj.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Pbj extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>I am a banana in this sandwich.</h1>
+      </div>
+    );
+  }
+}
+
+export default Pbj;
